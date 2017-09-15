c1da8cf865fc63a6abc5b5bf3aec4a5f71f79877
Kate Mashokha <katyamashokha@yahoo.com>
2017-09-15 11:49:04 -0700

    show found doctors based on symptom

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,6 +1,21 @@
+import { Doctor } from './../js/doctor.js';
+
 $(function() {
   $('#userInfo').submit(function(event){
     event.preventDefault();
     let issue = $('#symptom').val();
+    console.log(issue);
+
+    Doctor.apiRequest(issue, displayDoctors);
+
+
+
+    function displayDoctors(array) {
+      array.forEach(function(item){
+        $('#results').append("<li>" + item.name + '<p>' + item.address + 'Phone: ' + item.phone + 'Accepts new patients: ' + item.acceptsNewPatients + 'Website: ' + item.website);
+      });
+
+      $('input').val('');
+    }
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,7 +1,7 @@
-let apiKey = require('./../env').apiKey;
+let apiKey = require('./../.env').apiKey;
 
 export let Doctor = {
-  apiRequest: function(issue) {
+  apiRequest: function(issue, displayFunction) {
     $.ajax({
       url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=45.5231%2C%20-122.6765%2C20&skip=0&limit=10&user_key=${apiKey}`,
       type: 'GET',
@@ -9,7 +9,7 @@ export let Doctor = {
         format: 'json'
       },
       success: (response) => {
-        this.saveDoctorsToArray(response);
+        this.saveDoctorsToArray(response, displayFunction);
       },
       error: function() {
         $('#results').text('No doctors found');
@@ -17,17 +17,28 @@ export let Doctor = {
     });
   },
 
-  saveDoctorsToArray: function(response) {
+  saveDoctorsToArray: function(response, displayFunction) {
+    let that = this;
     let doctorsArray = [];
-    response.data.practices[0].forEach(function(doctor){
+    response.data[0].practices.forEach(function(doctor){
       doctorsArray.push(
         {
           name: doctor.name,
-          address: doctor.visit_address.street + ", " + doctor.visit_address.city + ", " + doctor.visit_address.zip,
+          address: doctor.visit_address.street + ", " + doctor.visit_address.city + ", " + doctor.visit_address.state + ", " + doctor.visit_address.zip,
           phone: doctor.phones[0].number,
           website: doctor.website,
-          acceptsNewPatients: doctor.accepts_new_patients
+          acceptsNewPatients: that.acceptsPatients(doctor.accepts_new_patients)
         });
       });
+      console.log(doctorsArray);
+      displayFunction(doctorsArray);
+    },
+
+    acceptsPatients(value) {
+      if (value === true) {
+        return 'Yes';
+      } else {
+        return 'No';
+      }
     }
   };
